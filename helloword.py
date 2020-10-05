47b9112e3f5e28bd591ea4cc47d982217363b7b2
abdulghaniy tohir <70440575+arsholl@users.noreply.github.com>
2020-10-05 15:42:06 +0100

    Add files via upload

---
+++ helloword.py
@@ -0,0 +1,50 @@
+# Day 1 - 30DaysOfPython Challenge
+
+#Open the python interactive shell and do the following operations. The operands are 3 and 4. Check the example above
+#addition(+)
+#multiplication(*)
+#modulus(%)
+#division(/)
+#exponential(**)
+#floor division operator(//).
+#solution
+print("\t\t\t\tsolution".capitalize())
+print(2 + 3)
+print(3 - 1)
+print(2 * 3)
+print(3 / 2)
+print(3 ** 2)
+print(3 % 2)
+print(3 // 2)
+
+#Write strings on the python interactive shell. The strings are the following:
+#Your name
+#Your family name
+#Your country
+#I am enjoying 30 days of python
+#solution
+print("\t\t\t\tsolution".capitalize())
+my_name = "Tohir"
+my_family_name = "Ile onileke"
+my_country = "Nigeria"
+enjoyment = "I am enjoying 30 days of python"
+print(f"{my_name}\n{my_family_name}\n{my_country}\n{enjoyment}")
+
+#Check the data types of the following data:
+#10
+#9.8
+#3.14
+#4 - 4j
+#['Asabeneh', 'Python', 'Finland']
+#Your name
+#Your family name
+#Your country
+#solution
+print("\t\t\t\tsolution".capitalize())
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Tohir', 'Ile onileke', 'Nigeria']))
+
+print("\t\t\t\tCONGRATULATIONS")
