6ef74f756a3ca325d0125e1c6cc2ade8aa1bd8b9
abdulghaniy tohir <70440575+arsholl@users.noreply.github.com>
2020-10-05 15:51:14 +0100

    Add files via upload
    how to built a multiplication table

---
+++ multiplication_table.py
@@ -0,0 +1,12 @@
+print("\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tMutiplication table\n")
+
+for i in range(1, 13):
+    print(i, end='\t\t\t')
+print()
+print("--------------------------------------------------------------------------------------------------------------------------------------\n")
+
+for j in range(1, 13):
+    for k in range(1, 13):
+        print(j * k, end='\t\t\t')
+    print('\n')
+
